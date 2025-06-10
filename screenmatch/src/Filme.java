d7c24496c3b397b1d35edd0c23220efac2eedbf5
Caio Luiz da Silva <caio.silva.3@aluno.cefet-rj.br>
2025-06-10 10:58:16 -0300

    add_sreenmacth

--- screenmatch/src/Filme.java
+++ screenmatch/src/Filme.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Filme {
+
+   String nome;
+   int anoDeLancamento;
+   int duracaoEmMinutos;
+   boolean incluidoNoPlano;
+   double somaDasAvaliacoes;
+   int totalDeAvaliacoes;
+
+   void exibeFichaTecnica() {
+      System.out.println("Nome do filme: " +nome);
+      System.out.println("Ano de lançamento: " +anoDeLancamento);
+      System.out.println("Duração em Minutos: " +duracaoEmMinutos);
+      System.out.println("Incluido no Plano: " +incluidoNoPlano);
+   }
+
+   void avalia(double nota) {
+      somaDasAvaliacoes += nota;
+      totalDeAvaliacoes++;
+   }
+
+   double pegaMedia() {
+      return somaDasAvaliacoes / totalDeAvaliacoes;
+   }
+
 }
