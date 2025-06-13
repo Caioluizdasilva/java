aca087b7425f3475405ae7dedce189c9e9652ada
Caio Luiz da Silva <caio.silva.3@aluno.cefet-rj.br>
2025-06-13 13:49:28 -0300

    add_filtro_recoemndacao

--- screenmatch/src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
+++ screenmatch/src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
@@ -1,4 +1,11 @@
 package br.com.alura.screenmatch.calculos;
 
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class FiltroRecomendacao {
+    private String recomendacao;
+
+    public void filtra(Classificavel classificavel){
+        
+    }
 }
